a177515bfe6ca813831b9aa3d2db91fb8bc16704
Luc Wallace <144689311+lucwl@users.noreply.github.com>
2025-12-01 05:23:52 +0000

    Add solution for day 01

--- day-01/main.py
+++ day-01/main.py
@@ -1,18 +1,48 @@
 with open("day-01/input.txt") as f:
     inputData = f.readlines()
 
+
 def q1() -> int:
-    a = 0
-    for line in inputData.split():
-        pass
-    return -1
+    counter = 50
+    total = 0
+    for line in inputData:
+        if line[0] == "L":
+            counter = (counter - int(line[1:])) % 100
+        elif line[0] == "R":
+            counter = (counter + int(line[1:])) % 100
+
+        if counter == 0:
+            total += 1
+
+    return total
+
 
 def q2() -> int:
-    return -1
+    counter = 50
+    total = 0
+    for line in inputData:
+        if line[0] == "L":
+            for _ in range(int(line[1:])):
+                counter -= 1
+                if counter == 0:
+                    total += 1
+                if counter == -1:
+                    counter = 99
+
+        elif line[0] == "R":
+            for _ in range(int(line[1:])):
+                counter += 1
+                if counter == 100:
+                    total += 1
+                    counter = 0
+
+    return total
+
 
 def main() -> None:
     print(q1())
     print(q2())
+
 
 if __name__ == "__main__":
     main()
